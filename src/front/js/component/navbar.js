b7a1e27975fe5a49618d368128bb0d84773a3aad
Gleisson Santos Silva <gtsasil@gmail.com>
2023-04-20 17:06:53 +0000

    Components card, country and navbar created

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">ShareTrip</span>
+        </Link>
+        <div className="ml-auto">
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <Link to="/demo">
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,58 +1,12 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Country } from "../component/country";
+import { Card } from "../component/card";
 
 export const Home = () => {
   return (
-    <div className="text-center mt-5">
-      <ol>
-        <li>
-          <Link to={"/country"}>
-            <a>Country</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/album"}>
-            <a>Album</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/favorites"}>
-            <a>Favorite</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/newpost"}>
-            <a>NewPost</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/notification"}>
-            <a>Notification</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/searchresult"}>
-            <a>SearchResult</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/userprofile"}>
-            <a>UserProfile</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link to={"/usersettings"}>
-            <a>UserSettings</a>
-          </Link>
-        </li>
-      </ol>
+    <div>
+      <Country />
+      <Card />
     </div>
   );
 };
