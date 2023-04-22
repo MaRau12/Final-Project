295b364480b56ffa8201ee287e4ec566f30a399a
Gleisson Santos Silva <gtsasil@gmail.com>
2023-04-22 09:45:33 +0000

    First update of userProfile

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,10 +1,8 @@
-
-import react from "react";
 import { Country } from "../component/country";
 import { Card } from "../component/card";
 
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+
 
 
 export const Home = () => {
--- src/front/js/pages/userProfile.js
+++ src/front/js/pages/userProfile.js
@@ -1,9 +1,40 @@
 import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
 
-export const UserProfile = ()  => {
-    return (
-        <div>
-            <h1>This is a User Profile Page</h1>
+export const UserProfile = () => {
+  return (
+    <div className="Container">
+      <div>
+        <div className="row justify-content-md-center">
+          <div
+            className="ratio ratio-1x1 rounded-circle overflow-hidden p-5 m-5   "
+            style={{ width: "14rem" }}
+          >
+            <img src={rigoImage} class="card-img-top rounded-circle" alt="" />
+          </div>
         </div>
-    )
-}
+        <p className="text-center">User name</p>
+      </div>
+      <div className="row justify-content-md-center">
+        <img
+          src={rigoImage}
+          className="card-img-top"
+          style={{ width: "16rem" }}
+          atl=""
+        />
+        <img
+          src={rigoImage}
+          className="card-img-top"
+          style={{ width: "16rem" }}
+          atl=""
+        />
+        <img
+          src={rigoImage}
+          className="card-img-top"
+          style={{ width: "16rem" }}
+          atl=""
+        />
+      </div>
+    </div>
+  );
+};
