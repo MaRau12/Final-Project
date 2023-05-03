c2619f0de8390093ef8ef6a5a852bf1acc5dca15
Gleisson Santos Silva <gtsasil@gmail.com>
2023-05-03 19:08:50 +0000

    Favorite&Album layout

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -3,50 +3,22 @@ import album from "../../img/album.jpg";
 import posts from "../../img/posts.jpg";
 import deepSea from "../../img/deepSea.jpg";
 import desert from "../../img/desert.jpg";
+import { Card } from "../component/card";
 
 export const Favorite = () => {
   return (
     <>
       <section className="jumbotron text-center">
         <div className="container">
-          <h1 className="jumbotron-heading">Album</h1>
+          <h1 className="jumbotron-heading">Favorite</h1>
           <p className="lead text-muted">
             Something short and leading about the collection of user photos.
           </p>
         </div>
       </section>
 
-      <div className="container">
-        <div className="album bg-light">
-          <div className="col ">
-            <div className="row d-flex justify-content-around row-cols-4 p-5">
-              <img
-                src={album}
-                alt="..."
-                class="img-thumbnail"
-                style={{ width: "18rem" }}
-              />
-              <img
-                src={posts}
-                alt="..."
-                class="img-thumbnail"
-                style={{ width: "18rem" }}
-              />
-              <img
-                src={desert}
-                alt="..."
-                class="img-thumbnail"
-                style={{ width: "18rem" }}
-              />
-              <img
-                src={deepSea}
-                alt="..."
-                class="img-thumbnail"
-                style={{ width: "18rem" }}
-              />
-            </div>
-          </div>
-        </div>
+      <div className="Container bg-light py-5">
+        <Card />
       </div>
     </>
   );
