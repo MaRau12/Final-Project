a21f40d3a73d0f756ed20ddc3c19ee0db5625391
SaraMoreiraG <sara_moreira@hotmail.es>
2023-05-17 15:53:45 +0000

    Jumbotron, buttons & NavBar

---
+++ src/front/js/component/buttons.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "../../styles/home.css";
+
+export const Buttons = () => {
+  return (
+    <>
+      <div className="buttons d-flex justify-content-center">
+        <button className="fill">Create</button>
+        <button className="pulse">Pulse</button>
+        <button className="close">Back</button>
+        <button className="raise">Raise</button>
+        <button className="up">Fill Up</button>
+        <button className="slide">More</button>
+        <button className="offset">Offset</button>
+      </div>
+    </>
+  );
+};
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,19 @@
-import React, { useContext, useState, } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
+import "../../styles/home.css";
+import logo from "../../img/logo.png";
+
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
-  
+
   const navigate = useNavigate();
 
-  const [transportName, setTransportName] = useState('');
-  const [searchPrice, setSearchPrice] = useState('');
-  const [searchFrom, setSearchFrom] = useState('');
-  const [searchTime, setSearchTime] = useState('');
-
+  const [transportName, setTransportName] = useState("");
+  const [searchPrice, setSearchPrice] = useState("");
+  const [searchFrom, setSearchFrom] = useState("");
+  const [searchTime, setSearchTime] = useState("");
 
   const handleInputChange1 = (event) => {
     setTransportName(event.target.value);
@@ -29,7 +31,9 @@ export const Navbar = () => {
     setSearchTime(event.target.value);
   };
   const handleSearch = async (e) => {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/transport_by_name/?name=${transportName}&price=${searchPrice}&from_location_search=${searchFrom}&travel_time=${searchTime}`);
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/transport_by_name/?name=${transportName}&price=${searchPrice}&from_location_search=${searchFrom}&travel_time=${searchTime}`
+    );
 
     const data = await response.json();
     actions.setSearchResults(data.posts);
@@ -37,102 +41,99 @@ export const Navbar = () => {
   };
 
   const handleLogout = async () => {
-    sessionStorage.removeItem('token');
-    navigate("/")
+    sessionStorage.removeItem("token");
+    navigate("/");
     window.location.reload();
-  }
+  };
 
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar">
       <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Home</span>
+        <Link to="/" className="no-underline">
+          {/* <img src={logo} className="logo" /> */}
+          <h1 className="navbar-brand">Travel Tales</h1>
         </Link>
         <div className="d-flex">
-          <div className="mb-3">
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-            >
-              Search Trips!
-            </button>
-            <div
-              className="modal fade"
-              id="exampleModal"
-              tabIndex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h1 className="modal-title fs-5" id="exampleModalLabel">
-                      Where do you want to go?
-                    </h1>
+          <button
+            className="pulse"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Search Trips
+          </button>
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    Where do you want to go?
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <input
+                    className="form-control me-2 mb-2"
+                    type="search"
+                    placeholder="Maximum Price"
+                    aria-label="Search"
+                    id="search-query"
+                    name="search-query"
+                    value={searchPrice}
+                    onChange={handleInputChange2}
+                  />
+                  <input
+                    className="form-control me-2 mb-2"
+                    type="search"
+                    placeholder="From or To City"
+                    aria-label="Search"
+                    id="search-query"
+                    name="search-query"
+                    value={searchFrom}
+                    onChange={handleInputChange3}
+                  />
+
+                  <input
+                    className="form-control me-2 mb-2"
+                    type="search"
+                    placeholder="Optional: Transport"
+                    aria-label="Search"
+                    id="search-query"
+                    name="search-query"
+                    value={transportName}
+                    onChange={handleInputChange1}
+                  />
+
+                  <input
+                    className="form-control me-2 mb-2"
+                    type="search"
+                    placeholder="Optional: Trip duration"
+                    aria-label="Search"
+                    id="search-query"
+                    name="search-query"
+                    value={searchTime}
+                    onChange={handleInputChange4}
+                  />
+                </div>
+                <div className="modal-footer">
+                  <Link to="/searchresult">
                     <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div className="modal-body">
-
-                    <input
-                      className="form-control me-2 mb-2"
-                      type="search"
-                      placeholder="Maximum Price"
-                      aria-label="Search"
-                      id="search-query"
-                      name="search-query"
-                      value={searchPrice}
-                      onChange={handleInputChange2}
-                    />
-                    <input
-                      className="form-control me-2 mb-2"
-                      type="search"
-                      placeholder="From or To City"
-                      aria-label="Search"
-                      id="search-query"
-                      name="search-query"
-                      value={searchFrom}
-                      onChange={handleInputChange3}
-                    />
-
-                    <input
-                      className="form-control me-2 mb-2"
-                      type="search"
-                      placeholder="Optional: Transport"
-                      aria-label="Search"
-                      id="search-query"
-                      name="search-query"
-                      value={transportName}
-                      onChange={handleInputChange1}
-                    />
-
-                    <input
-                      className="form-control me-2 mb-2"
-                      type="search"
-                      placeholder="Optional: Trip duration"
-                      aria-label="Search"
-                      id="search-query"
-                      name="search-query"
-                      value={searchTime}
-                      onChange={handleInputChange4}
-                    />
-                  </div>
-                  <div className="modal-footer">
-                    <Link to="/searchresult">
-                      <button
-                        className="btn btn-outline-success"
-                        onClick={handleSearch}
-                      >
-                        Search
-                      </button>
-                    </Link>
-                  </div>
+                      className="btn btn-outline-success"
+                      onClick={handleSearch}
+                    >
+                      Search
+                    </button>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -150,11 +151,16 @@ export const Navbar = () => {
                 <i className="fa-solid fa-circle-plus fa-2xl mp-0"></i>
               </button>
             </Link>
-            <button className="btn btn-outline-secondary mp-0" onClick={handleLogout}>Logout</button>
+            <button
+              className="btn btn-outline-secondary mp-0"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
           </>
         ) : (
           <Link to="/login">
-            <button className="btn btn-outline-primary">Login</button>
+            <button className="offset">LogIn</button>
           </Link>
         )}
       </div>
